e2ff3ba987e8a02668b5beadeb9a83869ed3fd45
Azzah <azzahafifahv@gmail.com>
2021-01-11 08:54:04 +0700

    udah bisa masuk database

---
+++ Statistisi/config.php
@@ -0,0 +1,10 @@
+<?php
+
+$databaseHost = 'localhost';
+$databaseName = 'db_dupak';
+$databaseUsername = 'root';
+$databasePassword = '';
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+
+?>
